146f64310287191bfd038607190a67b6641a394b
jmulins <114507954+jmulins@users.noreply.github.com>
2024-05-06 17:56:45 +0200

    Add files via upload

--- src/main/java/MonSimplexe.java
+++ src/main/java/MonSimplexe.java
@@ -1,5 +1,4 @@
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.*;
 
 public class MonSimplexe {
 
@@ -7,6 +6,15 @@ public class MonSimplexe {
 
     public static void calcule(double x1, double x2, double x3, double x4, double x5){
         double[][] simplexTableau = new double[7][12];
+
+        //AAAAAAAAAAAAAAAA
+//        prixMp.put("PORC", prixPorc);
+//        prixMp.put("CANARD", prixCanard);
+//        prixMp.put("POULET", prixPoulet);
+//        prixMp.put("PLASTIQUE", prixPlastique);
+//        prixMp.put("FER", prixFer);
+
+
         for (int i = 0; i < 5;i++){
             simplexTableau[i][i] = 1;
             //simplexTableau[i][i+5] = 1;
@@ -16,14 +24,39 @@ public class MonSimplexe {
             simplexTableau[i][i+5] = 1;
         }
 
+
+        simplexTableau[0][11] = donneMaxProductionPF(ProduitsFinis.TERRINE);
+        simplexTableau[1][11] = donneMaxProductionPF(ProduitsFinis.PATE);
+        simplexTableau[2][11] = donneMaxProductionPF(ProduitsFinis.MOUSSE);
+        simplexTableau[3][11] = donneMaxProductionPF(ProduitsFinis.JAMBON);
+        simplexTableau[4][11] = donneMaxProductionPF(ProduitsFinis.CUISSE);
+        simplexTableau[5][11] = 1;
+
+        simplexTableau[5][0] = 1/donneMaxProductionPF(ProduitsFinis.TERRINE);
+        simplexTableau[5][1] = 1/donneMaxProductionPF(ProduitsFinis.PATE);
+        simplexTableau[5][2] = 1/donneMaxProductionPF(ProduitsFinis.MOUSSE);
+        simplexTableau[5][3] = 1/donneMaxProductionPF(ProduitsFinis.JAMBON);
+        simplexTableau[5][4] = 1/donneMaxProductionPF(ProduitsFinis.CUISSE);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //        for (int i = 0; i < 11*5;i++){
 //            if (simplexTableau[i%5][i%12] !=  1){
 //                simplexTableau[i%5][i%12] = 0;
 //            }
 //
 //        }
-
-
 
 
 
@@ -88,16 +121,23 @@ public class MonSimplexe {
 
 
         switch (pf){
-            //case TERRINE ->
+            case TERRINE:
+                return Collections.min(Arrays.asList(nombreMachineDecoupe * 194400, nombreMachineBroyage * 487012, nombreMachineCuisson * 292207, nombreMachineEmballage * 259740));
+            case PATE:
+                return Collections.min(Arrays.asList(nombreMachineDecoupe * 395744, nombreMachineBroyage * 797872, nombreMachineCuisson * 574468, nombreMachineEmballage * 425531));
+            case MOUSSE:
+                return Collections.min(Arrays.asList(nombreMachineDecoupe * 116250, nombreMachineBroyage * 454545, nombreMachineCuisson * 606060, nombreMachineEmballage * 242424));
+                case JAMBON:
+                    Collections.min(Arrays.asList(nombreMachineDecoupe * 50000, nombreMachineCuisson * 181944, nombreMachineEmballage * 218780));
+            case CUISSE:
+                return Collections.min(Arrays.asList(nombreMachineDecoupe * 37500, nombreMachineEmballage * 78125));
 
 
         }
 
 
 
-
-
-        return 1d;
+        return 0;
 
     }
 
